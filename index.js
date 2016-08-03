6c64acb63db7b07cc4f8491f26f263774c172d09
zhangtiantian10 <975571814@qq.com>
2016-08-03 20:01:08 +0800

    implement add form

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ class View extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isEditor : true
+            isEditor: true,
+            formLists: []
         }
     }
 
@@ -12,11 +13,19 @@ class View extends React.Component {
         });
     }
 
+    addElement(type) {
+        let formLists = this.state.formLists;
+        formLists.push(type);
+        this.setState({
+            formLists: formLists
+        });
+    }
+
     render() {
         return <div>
             <button onClick={this.viewChange.bind(this)}>{this.state.isEditor ? "Preview" : "Edit"}</button>
             <div className={this.state.isEditor ? "" : "hidden"}>
-                <Editor/>
+                <Editor onAdd={this.addElement.bind(this)} formLists={this.state.formLists}/>
             </div>
             <div className={this.state.isEditor ? "hidden" : ""}>
                 <Preview/>
@@ -27,7 +36,38 @@ class View extends React.Component {
 
 class Editor extends React.Component {
     render() {
-        return <h1>Editor</h1>
+        return <div>
+            <RightButton onAdd={this.props.onAdd}/>
+            <LeftPanel formLists={this.props.formLists}/>
+        </div>;
+    }
+}
+
+class LeftPanel extends React.Component {
+    render() {
+        const formLists = this.props.formLists.map((formList, index) => {
+            return <div key={index}>
+                <input type={formList}/>
+                <button>X</button>
+            </div>
+        });
+
+        return <div>
+            {formLists}
+        </div>
+    }
+}
+
+class RightButton extends React.Component {
+    add(type) {
+        this.props.onAdd(type);
+    }
+
+    render() {
+        return <div>
+            <button onClick={this.add.bind(this, 'text')}>Text</button>
+            <button onClick={this.add.bind(this, 'date')}>Date</button>
+        </div>
     }
 }
 
