4242f8374c96481e1746ad998f4c2f876a93393c
zhangtiantian10 <975571814@qq.com>
2016-08-03 21:02:17 +0800

    implement add element

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ class View extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isEditor: true
+            isEditor: true,
+            elements: []
         }
     }
 
     viewChange() {
         this.setState({
-            isEditor: !this.state.isEditor
+            isEditor: !this.state.isEditor,
+        });
+    }
+
+    addElement(type) {
+        let elements = this.state.elements;
+        elements.push(type);
+        this.setState({
+            elements: elements
         });
     }
 
@@ -16,7 +25,7 @@ class View extends React.Component {
         return <div>
             <button onClick={this.viewChange.bind(this)}>{this.state.isEditor ? 'Preview' : 'Editor'}</button>
             <div className={this.state.isEditor ? "" : "hidden"}>
-                <Editor/>
+                <Editor onAdd={this.addElement.bind(this)} elements={this.state.elements}/>
             </div>
             <div className={this.state.isEditor ? "hidden" : ""}>
                 <Preview/>
@@ -28,7 +37,36 @@ class View extends React.Component {
 class Editor extends React.Component {
     render() {
         return <div>
-            Editor
+            <RightButton onAdd={this.props.onAdd}/>
+            <LeftPanel elements={this.props.elements}/>
+        </div>;
+    }
+}
+
+class RightButton extends React.Component {
+    add(type) {
+        this.props.onAdd(type);
+    }
+
+    render() {
+        return <div>
+            <button onClick={this.add.bind(this, 'text')}>Text</button>
+            <button onClick={this.add.bind(this, 'date')}>Date</button>
+        </div>;
+    }
+}
+
+class LeftPanel extends React.Component {
+    render() {
+        const elements = this.props.elements.map((element, index) => {
+            return <div key={index}>
+                <input type={element}/>
+                <button>X</button>
+            </div>
+        })
+
+        return <div>
+            {elements}
         </div>
     }
 }
